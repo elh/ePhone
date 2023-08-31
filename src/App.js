17c72b7b8cc38cb5fcb1a2a6e3bcfe06af6566f0
eugene huang <eugene@goforward.com>
2023-08-30 22:23:52 -0700

    get basic iframing on top of model working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { Environment, PresentationControls, useGLTF, Html } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+
+function Phone() {
+  const model = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf")
+  return (
+    <>
+      <Environment preset="warehouse" />
+      {/* <PresentationControls global polar={[-0.4, 0.2]} azimuth={[-0.4, 0.2]}> */}
+      <PresentationControls global>
+        {/* TODO: fix initial rotation */}
+        <primitive object={model.scene} position-y={-1.4}>
+          <Html wrapperClass='iframe-wrapper' position={[.17, 1.32, .082]} distanceFactor={1.068} transform>
+            <iframe src="https://elh.github.io" />
+          </Html>
+        </primitive>
+      </PresentationControls>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas camera={{
+        fov: 45,
+        near: 0.1,
+        far: 2000,
+        position: [-3, 1.5, 4]
+      }}>
+        <Phone></Phone>
+      </Canvas>
     </div>
   );
 }
