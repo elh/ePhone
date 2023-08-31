9abeec1a95d0183f491e53fb14b5f5c65e0726dd
eugene huang <eugene@goforward.com>
2023-08-30 22:59:04 -0700

    add suspense

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import { Environment, PresentationControls, useGLTF, Html } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react'
 
-function Phone() {
+function Phone({ url }) {
   const model = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf")
+
   return (
     <>
-      <Environment preset="warehouse" />
+      <Environment preset="night" />
       {/* <PresentationControls global polar={[-0.4, 0.2]} azimuth={[-0.4, 0.2]}> */}
       <PresentationControls global>
         {/* TODO: fix initial rotation */}
-        <primitive object={model.scene} position-y={-1.4}>
+        <primitive object={model.scene} position-y={-1.35}>
           <Html wrapperClass='iframe-wrapper' position={[.17, 1.32, .082]} distanceFactor={1.068} transform>
-            <iframe src="https://elh.github.io" />
+            <iframe src={url} title='ePhone screen' />
           </Html>
         </primitive>
       </PresentationControls>
@@ -23,13 +25,16 @@ function Phone() {
 function App() {
   return (
     <div className="App">
-      <Canvas camera={{
-        fov: 45,
-        near: 0.1,
-        far: 2000,
-        position: [-3, 1.5, 4]
-      }}>
-        <Phone></Phone>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 2000,
+          position: [-3, 1.5, 3.1]
+        }}>
+        <Suspense fallback={null}>
+          <Phone url="https://elh.github.io" />
+        </Suspense>
       </Canvas>
     </div>
   );
