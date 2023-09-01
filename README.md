672058216a0e661585c0787dd3993ee8e37f8c03
eugene huang <eugene@goforward.com>
2023-08-31 18:58:11 -0700

    support landscape mode

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # ePhone - iframe a site onto a phone model
 
-Built with `@react-three/fiber`.
+Built with `@react-three/fiber`. Drag to rotate the phone model.
 
-`url` query parameter can be used to specify the site to iframe. Drag to rotate the phone model.
+* `url` query parameter can be used to specify the site to iframe.
+* `landscape` query parameter can be used to rotate the phone. This works well with Youtube embeds.
 
 ![Wikipedia iframed onto a phone model](example.png)
 
@@ -13,5 +14,6 @@ Shout out Sam Osborne for the [tutorial](https://www.youtube.com/watch?v=SQRqU3N
 **TODO**
 - [ ] Add some info/help context
 - [ ] Add more of an environment. interactive?
-- [ ] Support landscape mode
+- [x] Support landscape mode
 - [ ] Manage link clicks inside iframe. Many sites will be blocked
+- [ ] Make this a lib. Could be used as a goofy Youtube player
--- src/App.js
+++ src/App.js
@@ -4,36 +4,54 @@ import { Canvas } from '@react-three/fiber';
 import { Suspense } from 'react'
 
 // Phone model with an iframe rendered over the screen
-function Phone({ url }) {
+function Phone({ url, landscape = false }) {
   // from market.pmnd.rs
-  const model = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf")
+  const model = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf");
 
-  return (
-    <>
-      <PresentationControls global polar={[-1, 0.4]}>
-        <primitive object={model.scene} position-y={-1.4} rotation={[-0.05, 0, 0]}>
-          {/* position and distanceFactor values I found for iphone-x/model.gltf */}
-          <Html wrapperClass='iframe-wrapper' position={[.17, 1.32, .091]} distanceFactor={1.068} transform occlude>
-            <iframe src={url} title='ePhone screen' />
-          </Html>
-        </primitive>
-      </PresentationControls>
-    </>
-  );
+  if (landscape) {
+    return (
+      <>
+        <PresentationControls global>
+          <primitive object={model.scene} position-y={0} position-x={1.2} rotation={[0, 0, Math.PI / 2]}>
+            {/* position and distanceFactor values I found for iphone-x/model.gltf */}
+            <Html wrapperClass='iframe-wrapper-landscape' position={[.17, 1.32, .091]} rotation={[0, 0, -Math.PI / 2]} distanceFactor={1.068} transform occlude>
+              <iframe src={url} title='ePhone screen - landscape' />
+            </Html>
+          </primitive>
+        </PresentationControls>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <PresentationControls global polar={[-1, 0.4]}>
+          <primitive object={model.scene} position-y={-1.4} rotation={[-0.05, 0, 0]}>
+            {/* position and distanceFactor values I found for iphone-x/model.gltf */}
+            <Html wrapperClass='iframe-wrapper' position={[.17, 1.32, .091]} distanceFactor={1.068} transform occlude>
+              <iframe src={url} title='ePhone screen' />
+            </Html>
+          </primitive>
+        </PresentationControls>
+      </>
+    );
+  }
 }
 
 function App() {
   const queryParameters = new URLSearchParams(window.location.search)
-  const url = queryParameters.get("url")
+  const url = queryParameters.get("url");
+  const landscape = queryParameters.get("landscape");
+
+  const cameraPos = landscape ? [0, -0.3, 3.3] : [0, 1, 4.4];
 
   return (
     <div className="App">
       <Canvas
-        camera={{ fov: 45, position: [0, 1, 4.4] }}>
+        camera={{ fov: 45, position: cameraPos }}>
         <Suspense fallback={null}>
           {/* <Environment preset="dawn" background blur={2} /> */}
           <Environment preset="night" />
-          <Phone url={url ? url : "https://elh.github.io"} />
+          <Phone url={url ? url : "https://elh.github.io"} landscape={!!landscape} />
         </Suspense>
       </Canvas>
     </div>
