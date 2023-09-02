002cc47bce5929343e3342e5e17c5bdf9b5c594c
eugene huang <eugene@goforward.com>
2023-09-02 13:06:00 -0700

    disable buttons and warn on mobile. zoom out

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function isValidHttpUrl(string) {
 
 // Phone model with an iframe rendered over the screen
 // Supports portrait and landscape orientations
-function Phone({ url, landscape = false, screenOff = false }) {
+function Phone({ url, landscape = false, disabled = false }) {
   // from market.pmnd.rs
   const model = useGLTF("https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/iphone-x/model.gltf");
 
   // phone orientation
-  const modelPos = landscape ? [-1.4, 0, 0] : [0, -1.4, 0];
+  const modelPos = landscape ? [-1.4, .2, 0] : [0, -1.4, 0];
   const modelRot = landscape ? [0, 0, -Math.PI / 2] : [-0.05, 0, 0];
   const iFrameWrapperClass = landscape ? 'iframe-wrapper-landscape' : 'iframe-wrapper';
   const iFrameWrapperRot = landscape ? [0, 0, Math.PI / 2] : [0, 0, 0];
@@ -34,7 +34,7 @@ function Phone({ url, landscape = false, screenOff = false }) {
       <PresentationControls global polar={[-1, 0.4]}>
         <primitive object={model.scene} position={modelPos} rotation={modelRot}>
           {/* position and distanceFactor values I found for iphone-x/model.gltf */}
-          {!screenOff &&
+          {!disabled &&
             <Html zIndexRange={[1000000, 0]} wrapperClass={iFrameWrapperClass} position={[.17, 1.32, .091]} rotation={iFrameWrapperRot} distanceFactor={1.068} transform occlude>
               <iframe src={url} title='ePhone screen' />
             </Html>
@@ -51,6 +51,9 @@ function App() {
   const [urlInput, setUrlInput] = useState(queryParameters.get("url"));
   const [landscape, setLandscape] = useState(!!queryParameters.get("landscape"));
   const [showInfo, setShowInfo] = useState(false);
+
+  const baseCameraPos = landscape ? [-.1, .1, 3.4] : [0, -.1, 4.5];
+  const cameraPos = isMobile ? (landscape ? [0, 0, 12] : [0, 0, 7]) : baseCameraPos;
 
   const rotate = () => {
     const windowURL = new URL(window.location.href);
@@ -84,7 +87,7 @@ function App() {
         <br />
         {showInfo &&
           <div className="bg-base-100 p-4">
-            <input type="text" placeholder="URL" className="input input-xs input-bordered w-80 focus:outline-0" value={urlInput} onChange={
+            <input type="text" placeholder="URL" className="input input-xs input-bordered w-80 focus:outline-0" value={urlInput} disabled={isMobile} onChange={
               (e) => { setUrlInput(e.target.value); }
             } onKeyDown={
               (e) => {
@@ -93,10 +96,10 @@ function App() {
                 }
               }
             } />
-            <button className="btn btn-neutral btn-xs mx-2" onClick={() => { goto(urlInput) }}>Go</button>
+            <button className="btn btn-neutral btn-xs mx-2" disabled={isMobile} onClick={() => { goto(urlInput) }}>Go</button>
             <br />
-            <button className="btn btn-xs mr-1 mt-1" onClick={() => { goto("https://en.wikipedia.org/wiki/IPhone") }}>→ Wiki</button>
-            <button className="btn btn-xs mr-1 mt-1" onClick={() => { goto("https://www.youtube.com/embed/_YUzQa_1RCE?si=Bbd79-yAvUR3sRtd") }}>→ Youtube</button>
+            <button className="btn btn-xs mr-1 mt-1" disabled={isMobile} onClick={() => { goto("https://en.wikipedia.org/wiki/IPhone") }}>→ Wiki</button>
+            <button className="btn btn-xs mr-1 mt-1" disabled={isMobile} onClick={() => { goto("https://www.youtube.com/embed/_YUzQa_1RCE?si=Bbd79-yAvUR3sRtd") }}>→ Youtube</button>
             <br />
             <br />
             <button className="btn btn-xs mr-1 my-1" onClick={rotate}>Change orientation</button>
@@ -104,15 +107,16 @@ function App() {
           </div>
         }
       </div>
+      {isMobile && <div className="p-4 text-center text-sm italic">Open on desktop to turn on the ePhone</div>}
       <div className='links'>
         <a href={`https://github.com/elh/ePhone`} className="link link-hover"><Github size={20} strokeWidth={2} /></a>
       </div>
       <Canvas>
         <Suspense fallback={null}>
-          <PerspectiveCamera makeDefault fov={45} position={landscape ? [-.1, .1, 3.4] : [0, -.1, 4.5]} />
+          <PerspectiveCamera makeDefault fov={45} position={cameraPos} />
           <Environment preset="night" />
           {/* If detected to be on mobile, don't iframe. Positioning is currently off and it's a bad experience anyways on a small screen */}
-          <Phone url={url ? url : "https://elh.github.io"} landscape={!!landscape} screenOff={isMobile} />
+          <Phone url={url ? url : "https://elh.github.io"} landscape={!!landscape} disabled={isMobile} />
         </Suspense>
       </Canvas>
     </div>
